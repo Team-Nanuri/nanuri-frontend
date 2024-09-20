3c360b347663ce3fa96e17c056deaf77d55f223e
Heeyeun Ko <koheeyeun96@gmail.com>
2024-09-20 20:40:43 +0900

    [feat]: 마이페이지 스타일링

--- src/user/pages/MyPage.tsx
+++ src/user/pages/MyPage.tsx
@@ -1,26 +1,59 @@
 import { useUser } from "../hooks/useUser";
-import { ActivityIcon } from "lucide-react";
+import styles from "./MyPage.module.css";
 
 export default function MyPage() {
   const {user, error} = useUser();
   
   return (
     <div className="w-full">
-   <ActivityIcon size={24}/>
+    <div className={styles.header}>
+    <img src={"/src/assets/profile.png"}/>
    {error && <p>Error loading user info</p>}
    {user ? (
-        <div>
-          {user.username} 
-          {user.userType}
+        <div className={styles.user}>
+          <label>{user.username} </label>
+          <label>{user.userType} </label>
         </div>
       ) : (
         <p>Loading user info...</p>
       )}
- 
-  <img src={"/src/assets/profile.png"}/>
-    <hr className="w-full bg-#F4F4F4  "></hr>
-  
     </div>
 
+    <hr className={styles.line}></hr>
+    <div className={styles.admin}>
+    <AdminButton label="나눔 물품 관리" />
+    <AdminButton label="회원 정보 관리" />
+    </div>
+    
+    <hr className={styles.line}></hr>
+    <div className={styles.setting}>
+    <SettingButton label="로그아웃"/>
+    <SettingButton label="탈퇴하기" />
+    <SettingButton label="개인정보처리방침" />
+    </div>
+    </div>
+
+  );
+}
+
+interface AdminButtonProps {
+  label: string;
+}
+
+
+export function AdminButton({ label }: AdminButtonProps) {
+  return (
+    <button className={styles.adminButton}>
+      {label}
+      <img src={"/src/assets/chevron-left.png"}/>
+    </button>
+  );
+}
+
+export function SettingButton({ label }: AdminButtonProps) {
+  return (
+    <button className={styles.settingButton}>
+    {label}
+    </button>
   );
 }
