c0d2571801602724bbc14df29bc6cc548d289ff8
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-09-21 21:15:23 +0900

    [Feat]: 대여, 나눔 선택 모달 추가

--- src/article/pages/ArticleSearchPage.tsx
+++ src/article/pages/ArticleSearchPage.tsx
@@ -1,14 +1,25 @@
-import {ChevronDown, ChevronLeft, Search} from "lucide-react";
-import {Input} from "@/global/components/ui/input.tsx";
+import {ChevronDown} from "lucide-react";
 import {Fragment, useState} from "react";
 import {Badge} from "@/global/components/ui/badge.tsx";
 import useArticlePaging from "@/article/hooks/useArticlePaging.ts";
 import ArticleItem from "@/article/components/ArticleItem.tsx";
-import {useNavigate} from "react-router-dom";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/global/components/ui/drawer"
+import {ShareType} from "@/article/api/article-response.ts";
+import ArticleSearchHeader from "@/article/components/ArticleSearchHeader.tsx";
+
 
 export default function ArticleSearchPage() {
   const [searchKeyword, setSearchKeyword] = useState("");
   const [apiKeyword, setApiKeyword] = useState<string | undefined>(undefined);
+  const [shareType, setShareType] = useState<ShareType | undefined>(undefined);
 
   const {
     data,
@@ -16,6 +27,7 @@ export default function ArticleSearchPage() {
     ref,
   } = useArticlePaging({
     keyword: apiKeyword,
+    shareType,
   });
 
   const onSearchClicked = () => {
@@ -30,7 +42,7 @@ export default function ArticleSearchPage() {
         setSearchKeyword={setSearchKeyword}
         onSearchClicked={onSearchClicked}
       />
-      <ArticleSearchParamBox/>
+      <ArticleSearchParamBox shareType={shareType} setShareType={setShareType}/>
       <section className="h-[calc(100%-100px)] overflow-auto">
         {data?.pages.map((page, i) => (
           <Fragment key={i}>
@@ -49,47 +61,10 @@ export default function ArticleSearchPage() {
   );
 }
 
-
-interface SearchHeaderProps {
-  searchKeyword: string;
-  setSearchKeyword: (keyword: string) => void;
-  onSearchClicked: () => void;
-}
-
-function ArticleSearchHeader({searchKeyword, setSearchKeyword, onSearchClicked}: SearchHeaderProps) {
-  const navigate = useNavigate();
-
-  const handleBack = () => {
-    navigate(-1); // 뒤로 가기
-  };
-
-  return (
-    <header className="
-      h-[60px] flex flex-row justify-between items-center
-      px-[20px] bg-white border-b
-    ">
-      <button onClick={handleBack}>
-        <ChevronLeft/>
-      </button>
-      <Input
-        className="mx-[12px] bg-searchBarGrey"
-        placeholder="검색어를 입력하세요"
-        value={searchKeyword}
-        onChange={(e) => setSearchKeyword(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            onSearchClicked();
-          }
-        }}
-      />
-      <button onClick={onSearchClicked}>
-        <Search/>
-      </button>
-    </header>
-  );
-}
-
-function ArticleSearchParamBox() {
+function ArticleSearchParamBox({shareType, setShareType}: {
+  shareType: ShareType | undefined,
+  setShareType: (shareType?: ShareType) => void
+}){
   return (
     <div
       className="
@@ -108,12 +83,7 @@ function ArticleSearchParamBox() {
         </div>
         <ChevronDown />
       </Badge>
-      <Badge variant="secondary" className="pr-1">
-        <div>
-          유형
-        </div>
-        <ChevronDown/>
-      </Badge>
+      <DrawerDemo shareType={shareType} setShareType={setShareType}/>
       <Badge variant="secondary" className="pr-1">
         <div>
           상태
@@ -121,5 +91,70 @@ function ArticleSearchParamBox() {
         <ChevronDown />
       </Badge>
     </div>
+  )
+}
+
+
+export function DrawerDemo(
+  {shareType, setShareType}: {
+    shareType: ShareType | undefined,
+    setShareType: (shareType?: ShareType) => void
+  }
+){
+  let label = '유형';
+  if(shareType === 'DONATE'){
+    label = '나눔';
+  }else if(shareType === 'RENTAL'){
+    label = '대여';
+  }
+
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Badge variant="secondary" className="pr-1">
+          <div>
+            {label}
+          </div>
+          <ChevronDown/>
+        </Badge>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>유형</DrawerTitle>
+          </DrawerHeader>
+
+          <DrawerFooter>
+            <div className="flex flex-row justify-between gap-4">
+              <DrawerClose asChild>
+                <button
+                  className="flex-[1] bg-[#f0f0f0] p-4 rounded-md"
+                  onClick={()=>setShareType(undefined)}
+                >
+                  취소
+                </button>
+              </DrawerClose>
+              <DrawerClose asChild>
+                <button
+                  className="flex-[1] bg-[#f0f0f0] p-4 rounded-md"
+                  onClick={()=>setShareType("DONATE")}
+                >
+                  나눔
+                </button>
+              </DrawerClose>
+              <DrawerClose asChild>
+                <button
+                  className="flex-[1] bg-[#f0f0f0] p-4 rounded-md"
+                  onClick={()=>setShareType("RENTAL")}
+                >
+                  대여
+                </button>
+              </DrawerClose>
+            </div>
+
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
   )
 }
