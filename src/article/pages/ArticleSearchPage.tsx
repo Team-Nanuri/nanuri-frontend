be73894b7b36f5567cc8fb2ff447bd49b45e0e00
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-09-21 11:56:12 +0900

    [Feat]: 검색 조건 헤더 추가

--- src/article/pages/ArticleSearchPage.tsx
+++ src/article/pages/ArticleSearchPage.tsx
@@ -1,6 +1,7 @@
-import {ChevronLeft, Search} from "lucide-react";
+import {ChevronDown, ChevronLeft, Search} from "lucide-react";
 import {Input} from "@/global/components/ui/input.tsx";
 import {useState} from "react";
+import {Badge} from "@/global/components/ui/badge.tsx";
 
 export default function ArticleSearchPage() {
   const [searchKeyword, setSearchKeyword] = useState("");
@@ -8,7 +9,7 @@ export default function ArticleSearchPage() {
   return (
     <div className="w-full h-full">
       <ArticleSearchHeader searchKeyword={searchKeyword} setSearchKeyword={setSearchKeyword}/>
-
+      <ArticleSearchParamBox/>
     </div>
   );
 }
@@ -25,14 +26,49 @@ function ArticleSearchHeader({searchKeyword, setSearchKeyword}: SearchHeaderProp
       h-[60px] flex flex-row justify-between items-center
       px-[20px] bg-white border-b
     ">
-      <ChevronLeft />
+      <ChevronLeft/>
       <Input
         className="mx-[12px] bg-searchBarGrey"
         placeholder="검색어를 입력하세요"
         value={searchKeyword}
         onChange={(e) => setSearchKeyword(e.target.value)}
       />
-      <Search />
+      <Search/>
     </header>
   );
+}
+
+function ArticleSearchParamBox() {
+  return (
+    <div
+      className="
+      h-[40px] flex flex-row justify-start
+      items-center px-[20px] gap-[10px]
+    ">
+      <Badge variant="secondary" className="pr-1">
+        <div>
+          카테고리
+        </div>
+        <ChevronDown />
+      </Badge>
+      <Badge variant="secondary" className="pr-1">
+        <div>
+          정렬
+        </div>
+        <ChevronDown />
+      </Badge>
+      <Badge variant="secondary" className="pr-1">
+        <div>
+          유형
+        </div>
+        <ChevronDown/>
+      </Badge>
+      <Badge variant="secondary" className="pr-1">
+        <div>
+          상태
+        </div>
+        <ChevronDown />
+      </Badge>
+    </div>
+  )
 }
