e317801bf75712dc3a492c06e88df3c5c1c8e100
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-09-21 22:28:43 +0900

    [Feat]: 시간비교 함수 수정

--- src/chat/utils/chat-utils.ts
+++ src/chat/utils/chat-utils.ts
@@ -1,17 +1,29 @@
 import {MessageModel} from "@/chat/api/chat-response.ts";
 
+function compareDate(date1: Date, date2: Date) {
+  return date1.getHours() === date2.getHours()
+    && date1.getMinutes() === date2.getMinutes();
+}
+
 export function shouldShowTime(messages : MessageModel[], index: number, message: MessageModel) {
   const date = new Date(message.createdAt);
 
   const isSingleMessage = messages.length === 1;
   const isFirstMessage = index === 0;
   const isLastMessage = index === messages.length - 1;
-  const isNextTimeDifferent = isFirstMessage && messages.length > 1 && message.createdAt !== messages[1].createdAt;
-  const isOtherDayAndNextTimeEqual = !isFirstMessage && !isLastMessage && new Date(messages[index-1].createdAt).getDay() !== date.getDay() && message.createdAt === messages[index + 1].createdAt;
+  const isNextTimeDifferent = isFirstMessage && messages.length > 1
+    && message.createdAt !== messages[1].createdAt;
+  const isOtherDayAndNextTimeEqual = !isFirstMessage
+    && !isLastMessage
+    && new Date(messages[index-1].createdAt).getDay() !== date.getDay()
+    && compareDate(date, new Date(messages[index + 1].createdAt));
+
   if(isOtherDayAndNextTimeEqual){
     return false;
   }
-  const isPrevTimeDifferent = index > 0 && message.createdAt !== messages[index - 1].createdAt;
+  const isPrevTimeDifferent = index > 0
+    && !compareDate(date, new Date(messages[index - 1].createdAt));
+
 
   return isSingleMessage || isNextTimeDifferent || isPrevTimeDifferent || isLastMessage;
 }
