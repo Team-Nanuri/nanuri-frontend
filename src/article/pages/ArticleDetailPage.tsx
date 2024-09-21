8111799c98f1ed75392752ba4a4bf097276e2c7a
Heeyeun Ko <koheeyeun96@gmail.com>
2024-09-22 02:21:11 +0900

    [feat]: Detail page 구분선 수정

--- src/article/pages/ArticleDetailPage.tsx
+++ src/article/pages/ArticleDetailPage.tsx
@@ -3,13 +3,13 @@ import { useQuery } from "@tanstack/react-query";
 import { getArticleDetail } from "../api/article-api";
 import styles from "./ArticleDetailPage.module.css";
 import { ChevronLeft, Heart } from "lucide-react";
-import dayjs from 'dayjs';
-import 'dayjs/locale/ko';
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
 import LoadingSpinner from "@/global/components/LoadingSpinner";
+import { ArticleDetailModel } from "../api/article-response";
 // import styles from "@/article/pages/ArticleDetailPage.module.css";
 
 export default function ArticleDetailPage() {
-    const navigate = useNavigate();
   const { articleId } = useParams<{ articleId: string }>();
   const { data: articleDetail } = useQuery({
     queryKey: ["article", articleId],
@@ -18,68 +18,81 @@ export default function ArticleDetailPage() {
     },
   });
 
+  if (!articleDetail) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <div className="w-full h-full">
+      <ArticleDetailHeader />
+      {/* imageUrls 배열을 map으로 순회하여 이미지를 렌더링 */}
+      <div className={styles.picturesContainer}>
+        {articleDetail?.imageUrls?.map((imageUrl: string, index: number) => (
+          <img
+            key={index}
+            className={styles.pictures}
+            src={imageUrl}
+            alt={`image-${index}`}
+          />
+        ))}
+      </div>
+      <ArticleDetailContent articleDetail={articleDetail} />
+    </div>
+  );
+}
+
+function ArticleDetailHeader() {
+    
+  const navigate = useNavigate();
 
   const goBack = () => {
     navigate(-1);
   };
 
-  if(!articleDetail) {
-    return <LoadingSpinner />
-  }
-
-  const dateStr = articleDetail.createdAt;
-  dayjs.locale('ko');
-  const formatDateStr = dayjs(dateStr).format('YYYY/MM/DD HH:mm');
-
-
   return (
-    <div className="w-full h-full">
-         <header className={styles.header}>
-        <button className={styles.backButton} onClick={goBack}>
-        <ChevronLeft/>
-        </button>
-
-        <button className={styles.heartButton}>
-         <Heart />
-        </button>
-      </header>
-      {/* imageUrls 배열을 map으로 순회하여 이미지를 렌더링 */}
-      <div className={styles.picturesContainer}>
-      {articleDetail?.imageUrls?.map((imageUrl: string, index: number) => (
-        <img
-          key={index}
-          className={styles.pictures}
-          src={imageUrl}
-          alt={`image-${index}`}
-        />
-      ))}
-      </div>
-      <div className={styles.contentContainer}> 
-      <div className={styles.userContainer}>
-        <p className={styles.username}>{articleDetail?.writer.username}</p>
-        <p className={styles.userType}>{articleDetail?.writer.userType}</p>
-        </div>
-        <hr className={styles.line}/>
-        <div className={styles.infoContainer}>
-        <div className={styles.infoFirstLine}>
-        <p className={styles.title}>{articleDetail?.title}</p>
-        <p className={styles.category}>{articleDetail?.category}</p>
-        </div>
-        <p className={styles.createdAt}>{formatDateStr}</p>
-        <p className={styles.shareType}>{articleDetail?.shareType}</p>
-        </div>
-        <div className={styles.contentWrapper}>
-        <p className={styles.content}>{articleDetail?.content}</p>
-       </div>
-        <footer>
-            <button className={styles.footerButton}>문의</button>
-        </footer>
-
-        </div>
-
-   
-        </div>
-    
+    <header className={styles.header}>
+      <button className={styles.backButton} onClick={goBack}>
+        <ChevronLeft />
+      </button>
+      <button className={styles.heartButton}>
+        <Heart />
+      </button>
+    </header>
   );
 }
 
+function ArticleDetailContent({
+  articleDetail,
+}: {
+  articleDetail: ArticleDetailModel;
+}) {
+  const dateStr = articleDetail.createdAt;
+  dayjs.locale("ko");
+  const formatDateStr = dayjs(dateStr).format("YYYY/MM/DD HH:mm");
+
+  return (
+    <>
+      <div className={styles.contentContainer}>
+        <div className={styles.userContainer}>
+          <p className={styles.username}>{articleDetail?.writer.username}</p>
+          <p className={styles.userType}>{articleDetail?.writer.userType}</p>
+        </div>
+        <hr className={styles.line} />
+        <div className={styles.infoContainer}>
+          <div className={styles.infoFirstLine}>
+            <p className={styles.title}>{articleDetail?.title}</p>
+            <p className={styles.category}>{articleDetail?.category}</p>
+          </div>
+          <p className={styles.createdAt}>{formatDateStr}</p>
+          <p className={styles.shareType}>{articleDetail?.shareType}</p>
+        </div>
+        <div className={styles.contentWrapper}>
+          <p className={styles.content}>{articleDetail?.content}</p>
+        </div>
+        <footer>
+          <button className={styles.footerButton}>문의</button>
+        </footer>
+      </div>
+    </>
+  );
+}
