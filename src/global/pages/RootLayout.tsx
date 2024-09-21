ca96d9718fee0e205eb447bb10529f1c0b8ae6e6
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-09-21 17:04:41 +0900

    [Feat]: 권한 관리

--- src/global/pages/RootLayout.tsx
+++ src/global/pages/RootLayout.tsx
@@ -1,8 +1,26 @@
-import {Outlet} from "react-router-dom";
-import {Suspense} from "react";
+import {Outlet, useLocation, useNavigate} from "react-router-dom";
+import {Suspense, useEffect} from "react";
 import {ErrorBoundary} from "react-error-boundary";
+import {useUser} from "@/user/hooks/useUser.ts";
 
 export default function RootLayout() {
+
+  const {user, error} = useUser();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isAuthPath = location.pathname === "/login" || location.pathname === "/signup";
+  const isLoginPath = !isAuthPath;
+
+  useEffect(() => {
+    console.log(`RootLayout useEffect ${location.pathname} user:${user} e:${error} au: ${isAuthPath} lo: ${isLoginPath}`);
+    if(user && isAuthPath) {
+      navigate("/");
+    }
+    if(error && isLoginPath) {
+      navigate("/login");
+    }
+
+  }, [user, error, location.pathname]);
 
   return (
     <div className="w-full h-dvh flex justify-center bg-bgCanvasWhite overflow-y-auto overflow-x-hidden">
