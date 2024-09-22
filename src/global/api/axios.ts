9113bbfdc215a8064690b66e079d2869fc947e2e
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-09-22 15:01:07 +0900

    [Feat]: 토큰

--- src/global/api/axios.ts
+++ src/global/api/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
-import {API_BASE_URL} from "@/global/const/const.ts";
+import {ACCESS_TOKEN, API_BASE_URL, REFRESH_TOKEN} from "@/global/const/const.ts";
+import {ApiError} from "@/global/api/response.ts";
+import {useQueryClient} from "@tanstack/react-query";
 
 export const axiosClient = axios.create({
   baseURL: API_BASE_URL,
@@ -8,3 +10,66 @@ export const axiosClient = axios.create({
     'Cross-Control-Allow-Origin': '*',
   },
 });
+
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+
+axiosClient.interceptors.response.use(
+  (response) => {
+    return {
+      ...response,
+    }
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+      if (!refreshToken) {
+        return Promise.reject(error);
+      }
+      const resp = await fetch(`${API_BASE_URL}/api/auth/refresh`, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'Cross-Control-Allow-Origin': '*',
+          'Authorization': `Bearer ${refreshToken}`,
+        },
+      });
+      if (resp.ok) {
+        console.log('토큰 재발급 성공');
+        const res = await resp.json();
+        localStorage.setItem(ACCESS_TOKEN, res.data.accessToken);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refreshToken);
+        return axiosClient(originalRequest);
+      }else{
+        console.log('토큰 재발급 실패');
+        localStorage.removeItem(ACCESS_TOKEN);
+        localStorage.removeItem(REFRESH_TOKEN);
+
+        const queryClient = useQueryClient();
+        queryClient.removeQueries({
+          queryKey: ['userInfo'],
+        });
+      }
+      return Promise.reject(error);
+    }
+    // 응답 body에서 받은 에러코드와 메시지로 에러 객체 생성 후 throw
+    const resBody = error.response.data;
+    if(resBody.result === 'FAIL') {
+      throw new ApiError(error.response.status,resBody.message);
+
+    }
+    return Promise.reject(error);
+  },
+);
